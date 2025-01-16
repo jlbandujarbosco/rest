7f08bf6a8b3bb406f30b5a1d9438db93685e83ac
JL <JL@gmail.com>
2025-01-16 15:56:13 +0000

    v2-lista-productos

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express');
 require('dotenv').config();
 const app = express();
+app.use(express.static('public'));
+app.use(express.json()); // Middleware para analizar JSON
 const port = process.env.PORT || 80;
-const producto = {
+const listaProductos = [{
     id: 1,
     nombre: 'Laptop',
     precio: 1000
-};
+},{
+    id: 2,
+    nombre: 'Sobremesa',
+    precio: 2000
+}];
 app.get('/producto', (req, res) => {
-    res.json(producto);
+    res.json(listaProductos);
 });
+
+app.post('/categoria-color', (req, res) => {
+    const { categoria, color } = req.body;
+    res.send( `Categoría: ${categoria}, Color: ${color}`);
+});
+
 app.listen(port, () => {
     console.log(
 `Servidor corriendo en puerto:${port}`);
